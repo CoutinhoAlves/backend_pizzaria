507d9ed2bcc379bd0ecd5432e828c13924f0e302
COUTINHO ALVES <couto.alves@outlook.com>
2026-01-09 11:59:56 -0300

    Criando validação schema

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 import { CreateuUserController } from './controllers/user/CreateUserController';
+import { validateSchema } from './middlewares/validateSchema';
+import { createUserSchema } from './schemas/userSchema';
 
 const router = Router();
 
-router.post('/users', new CreateuUserController().handler);
+router.post('/users', validateSchema(createUserSchema), new CreateuUserController().handler);
 
 
 export { router };
---
+++ src/schemas/userSchema.ts
@@ -0,0 +1,13 @@
+import { email, z } from 'zod';
+
+export const createUserSchema = z.object({
+    body: z.object({
+        name: z
+            .string({message: "O nome precisa ser um texto"})
+            .min(3, {message: "O nome precisa ter no mínimo 3 caracteres"}),
+        email: email({message: "O email precisa ser válido"}),
+        password: z
+            .string({message: "A senha precisa ser conter caractere de exemplo @#123"})
+            .min(6, {message: "A senha precisa ter no mínimo 6 caracteres"}),
+    })
+})
